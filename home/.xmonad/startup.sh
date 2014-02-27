78f86b5e2fd45f3925713930bb202b60a6774628
David Simon <david.mike.simon@gmail.com>
2014-02-27 15:25:22 -0500

    Switching to qtile

--- home/.xmonad/startup.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-
-export WINDOW_MANAGER=/home/dave/bin/xmonad
-gnome-session
--- required-apt.sh
+++ required-apt.sh
@@ -1,2 +1,4 @@
 #!/bin/sh
-sudo apt-get install dtrx tree vim vim-gnome cscope vim-doc zsh ack ctags git xsel
+sudo apt-add-repository ppa:tycho-s/ppa
+sudo apt-get update
+sudo apt-get install dtrx tree vim vim-gnome cscope vim-doc zsh ack ctags git xsel qtile
