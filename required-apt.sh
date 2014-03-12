5bc4bedd61863f586fc38d3a31d473d9c9a4cbc6
David Simon <david.mike.simon@gmail.com>
2014-03-12 13:53:13 -0400

    Added volti, tracking .config directly now

--- required-apt.sh
+++ required-apt.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 sudo apt-add-repository ppa:tycho-s/ppa
 sudo apt-get update
-sudo apt-get install dtrx tree vim vim-gnome cscope vim-doc zsh ack ctags git xsel qtile suckless-tools xcompmgr
+sudo apt-get install dtrx tree vim vim-gnome cscope vim-doc zsh ack ctags git xsel qtile suckless-tools xcompmgr volti
