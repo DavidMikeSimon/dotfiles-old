4f02d234dc2f0a627d1cd51e5445f3eeb58b65ce
David Simon <david.mike.simon@gmail.com>
2015-03-10 17:12:19 -0400

    Improved taffybar styling

--- home/.config/taffybar/taffybar.hs
+++ home/.config/taffybar/taffybar.hs
@@ -1,6 +1,7 @@
 import System.Taffybar
 import System.Taffybar.Systray
 import System.Taffybar.TaffyPager
+import System.Taffybar.Pager
 import System.Taffybar.SimpleClock
 import System.Taffybar.Widgets.PollingGraph
 import System.Taffybar.MPRIS
@@ -13,6 +14,14 @@ cpuCallback = do
 
 graphColors = [(0,1,0,1), (1,0,1,0.5)]
 
+iconifyLayout :: String -> String
+iconifyLayout name
+  | name == "Tall" = icon "\9703"
+  | name == "Mirror Tall" = icon "\9636"
+  | name == "Full" = icon "\9635"
+  | otherwise = name
+  where icon = \s -> "<span size='x-large' rise='8000'>" ++ s ++ "</span>"
+
 main = do
   defaultTaffybar defaultTaffybarConfig {
     startWidgets = [ pager ],
@@ -21,9 +30,19 @@ main = do
     monitorNumber = 0
   }
   where cpuCfg = defaultGraphConfig { graphDataColors = graphColors, graphLabel = Just "cpu" }
-        clock = textClockNew Nothing "<span fgcolor='orange'>%a %b %_d %H:%M</span>" 1
-        pager = taffyPagerNew defaultPagerConfig
+        clock = textClockNew Nothing (colorize "orange" "" "%a %b %_d %H:%M") 1
         tray = systrayNew
         cpu = pollingGraphNew cpuCfg 0.5 cpuCallback
         mpris = mprisNew
         notify = notifyAreaNew defaultNotificationConfig
+        pager = taffyPagerNew pagerConfig
+        pagerConfig = PagerConfig {
+            activeWindow = escape . shorten 100,
+            activeLayout = iconifyLayout,
+            activeWorkspace = colorize "yellow" "" . escape,
+            hiddenWorkspace = escape,
+            emptyWorkspace = colorize "grey" "" . escape,
+            visibleWorkspace = colorize "green" "" . escape,
+            urgentWorkspace = colorize "red" "yellow",
+            widgetSep = "  "
+          }
